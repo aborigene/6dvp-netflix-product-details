28d8bf71be73c0e70b49744a16db94a1b756eb8f
Igor Simoes <igor.simoes@dynatrace.com>
2021-03-29 22:58:05 -0300

    movie_search

--- src/main/java/com/dvp6/grupo1/product_details/MovieRepository.java
+++ src/main/java/com/dvp6/grupo1/product_details/MovieRepository.java
@@ -11,13 +11,27 @@ public interface MovieRepository extends CrudRepository<Movie, String>  {
     List<Movie> findByTitle(String title);
     List<Movie> findByType(String type);
     Optional<Movie> findById(String imdbid);
-    //@Query("SELECT m FROM Movie m WHERE LOWER(m.title) like '%LOWER(:title)%'")
-    List<Movie> findByTitleContaining(String title);
+    
     List<Movie> findByYear(String year);
     List<Movie> findByTitleAndYear(String title, String year);
     @Query("SELECT m FROM Movie m WHERE LOWER(m.title) like '%LOWER(:title)%' and m.year = ':year'")
     List<Movie> searchByTitleAndYear(String title, String year);
     List<Movie> findByGenre(String genre);
+
+    List<Movie> findTopByTitleContaining(String title);
+    List<Movie> findTop5ByTitleContaining(String title);
+    List<Movie> findTop10ByTitleContaining(String title);
+    List<Movie> findTop20ByTitleContaining(String title);
+
+    List<Movie> findTopByTitleContainingOrderByLikes(String title);
+    List<Movie> findTop5ByTitleContainingOrderByLikes(String title);
+    List<Movie> findTop10ByTitleContainingOrderByLikes(String title);
+    List<Movie> findTop20ByTitleContainingOrderByLikes(String title);
+
+    List<Movie> findTopByTitleContainingOrderByDislikes(String title);
+    List<Movie> findTop5ByTitleContainingOrderByDislikes(String title);
+    List<Movie> findTop10ByTitleContainingOrderByDislikes(String title);
+    List<Movie> findTop20ByTitleContainingOrderByDislikes(String title);
 
     List<Movie> findTopByGenreContaining(String genre);
     List<Movie> findTop5ByGenreContaining(String genre);
--- src/main/java/com/dvp6/grupo1/product_details/ProductDetailsController.java
+++ src/main/java/com/dvp6/grupo1/product_details/ProductDetailsController.java
@@ -81,4 +81,69 @@ public class ProductDetailsController {
 		
 		return new ResponseEntity<String>(new Gson().toJson(movie), HttpStatus.OK);
 	}    
+
+	@RequestMapping(method = RequestMethod.GET, value = "/api/movie_search")
+    @ResponseBody
+	public ResponseEntity<String> moviesSearch(@RequestParam(name = "title_search") String title_search, @RequestParam(name = "top", required = false, defaultValue = "1") Integer top, @RequestParam(name = "order_by") String order_by) {
+		List<Movie> movie;
+		if(order_by.equals("likes")){
+			switch(top){
+				case 5:
+					movie = movieRepository.findTop5ByTitleContainingOrderByLikes(title_search);
+					break;
+				case 10:
+					movie = movieRepository.findTop10ByTitleContainingOrderByLikes(title_search);
+					break;
+				case 20: 
+					movie = movieRepository.findTop20ByTitleContainingOrderByLikes(title_search);
+					break;
+				default:
+					movie = movieRepository.findTopByTitleContainingOrderByLikes(title_search);
+			}
+		}
+		else{
+			if(order_by.equals("dislikes")){
+				switch(top){
+					case 5:
+						movie = movieRepository.findTop5ByTitleContainingOrderByDislikes(title_search);
+						break;
+					case 10:
+						movie = movieRepository.findTop10ByTitleContainingOrderByDislikes(title_search);
+						break;
+					case 20: 
+						movie = movieRepository.findTop20ByTitleContainingOrderByDislikes(title_search);
+						break;
+					default:
+						movie = movieRepository.findTopByTitleContainingOrderByDislikes(title_search);
+				}			
+			}
+			else{
+				if(order_by.equals("none")){
+					switch(top){
+						case 5:
+							movie = movieRepository.findTop5ByTitleContaining(title_search);
+							break;
+						case 10:
+							movie = movieRepository.findTop10ByTitleContaining(title_search);
+							break;
+						case 20: 
+							movie = movieRepository.findTop20ByTitleContaining(title_search);
+							break;
+						default:
+							movie = movieRepository.findTopByTitleContaining(title_search);
+					}
+				}
+				else{
+					Map<String, String> responseMessage = new HashMap<String,String>();
+					responseMessage.put("erroCode",HttpStatus.BAD_REQUEST.toString());
+					responseMessage.put("Message","Wrong order_by parameter informed. Please inform likes, dislikes or none. Anything else will trhow an error.");
+					return new ResponseEntity<String>(new Gson().toJson(responseMessage), HttpStatus.BAD_REQUEST);
+				}
+			}
+		}
+		
+
+		return new ResponseEntity<String>(new Gson().toJson(movie), HttpStatus.OK);
+
+	}	
 }
